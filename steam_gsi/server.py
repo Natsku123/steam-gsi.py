d66fe7a3baaa9f1205837b5ccea1e600ada26b9c
Max Mecklin <max@meckl.in>
2021-06-20 13:06:59 +0300

    Move server code into a class

--- steam_gsi/server.py
+++ steam_gsi/server.py
@@ -1,17 +1,39 @@
+import asyncio
+import signal
 from aiohttp import web
 from aiohttp.web import Request, Response
 
 from .logs import logger
 
 
-async def gsi_root(request: Request):
-    content = await request.json()
-    logger.debug(content)
-    return Response(status=200)
+class Server:
+    def __init__(self, host: str = 'localhost', port: int = 3000):
+        self.__host = host
+        self.__port = port
+        self.__app = web.Application()
 
-app = web.Application()
-app.add_routes([web.post('/', gsi_root)])
+        self.__app.add_routes([web.post('/', self.gsi_handler)])
 
+    async def gsi_handler(self, request: Request):
+        content = await request.json()
+        logger.debug(f"Incoming event: {content}")
+        return Response(status=200)
 
-def run():
-    web.run_app(app)
+    async def run(self):
+        # Raise KeyboardInterrupt on SIGTERM
+        def handle_sigterm(sig, frame):
+            raise KeyboardInterrupt
+
+        signal.signal(signal.SIGTERM, handle_sigterm)
+
+        runner = web.AppRunner(self.__app)
+        await runner.setup()
+        site = web.TCPSite(runner, self.__host, self.__port)
+        await site.start()
+
+        # Block until KeyboardInterrupt is detected
+        try:
+            while True:
+                await asyncio.sleep(3600)
+        except KeyboardInterrupt:
+            await runner.cleanup()
