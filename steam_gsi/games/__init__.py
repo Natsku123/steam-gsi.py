89023c30c92732fbbc2a1fc069846a41528cc778
Max Mecklin <max@meckl.in>
2021-06-28 11:28:10 +0300

    JSON to dataclass serialization

--- steam_gsi/games/__init__.py
+++ steam_gsi/games/__init__.py
@@ -1,5 +1,6 @@
 import json
-from dataclasses import dataclass, asdict
+from typing import Union, Dict
+from dataclasses import dataclass, asdict, fields
 
 
 class SerializationError(Exception):
@@ -21,13 +22,42 @@ class Base:
         return json.dumps(asdict(self))
 
     @classmethod
-    def from_json(cls, data: str):
+    def from_json(cls, data: Union[str, Dict]):
         """
-        Converts JSON string to object
+        Converts JSON string or dict to object
 
-        :param data: JSON string
+        :param data: JSON string or dict
         :return: new instance of class
-        :raises TypeError when data has more unknown attributes
+        :raises SerializationError when data has unknown attributes
         """
-        return cls(**json.loads(data))
+
+        # load JSON if string
+        if isinstance(data, str):
+            new_data = json.loads(data)
+
+        # just use dict if it is already a dict
+        elif isinstance(data, dict):
+            new_data = data
+
+        # If somehow unsupported type, use empty dict
+        else:
+            new_data = {}
+
+        # Dataclasses do not support keyword arguments so we have to convert
+        # them into positional through a list and a check that they exist
+        args = []
+
+        for k in fields(cls):
+            if k.name not in new_data:
+                raise SerializationError("Missing attribute: " + k.name)
+
+            # Call recursively the serialization of a sub object, if necessary
+            if issubclass(Base, k.type):
+                v = k.type.from_json(new_data[k.name])
+            else:
+                v = new_data[k.name]
+
+            args.append(v)
+
+        return cls(*args)
 
